0ad12dd570d9e9b61a087d06e79e68735e79ed22
Tosin Omotayo <tosin.omotayo@fintraksoftware.com>
2020-01-13 11:42:30 +0100

    Added trainee card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'somethingawesome';
+ name = 'Tosin Omotayo';
+ tvShow = 'Suits';
+academy = [
+  {name: 'Tosin', image: 'small.jpg', complexion: 'black', hobbies: ['Suits','Arsenal']},
+  {
+    name: 'Zino', image: 'small.jpg', complexion: 'black', hobbies: ['Dancing']
+  },
+  { name : 'Sarah', image: 'small.jpg', complexion: 'yellow', hobbies: ['Singing']
+},
+{ name : 'Najeeb', image: 'small.jpg', complexion: 'yellow', hobbies: ['Singing']
+},
+{ name : 'Charlse', image: 'small.jpg', complexion: 'yellow', hobbies: ['Singing']
 }
+];
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { Whatever } from './app.whatever.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Whatever,
+    AlbumComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
